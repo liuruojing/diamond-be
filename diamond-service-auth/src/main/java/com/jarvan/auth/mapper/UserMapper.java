21d2db2b8a085a6237a39d86d3b5778e573ce26e
liuruojing <liuruojing@richstonedt.com>
2019-03-29 18:06:02 +0800

    feat:add mybatis-plus to create controller、service、mapper

---
+++ diamond-service-auth/src/main/java/com/jarvan/auth/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.jarvan.auth.mapper;
+
+import com.jarvan.auth.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author liuruojing
+ * @since 2019-03-29
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
